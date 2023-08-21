ff45007a455156c41d559517c7603042a8dbfe0b
Marin <marincovic4@gmail.com>
2023-08-21 17:13:33 +0200

    fixed avatar logo

--- src/components/Header/ProfileAvatar.jsx
+++ src/components/Header/ProfileAvatar.jsx
@@ -1,9 +1,10 @@
 import { Avatar, ListItemButton } from "@mui/material";
+import AvatarImg from "../../assets/avatar.svg";
 
 export const ProfileAvatar = () => {
   return (
     <ListItemButton sx={{ padding: "0" }} disableRipple>
-      <Avatar alt="profile picture" src="./src/assets/avatar.svg" />
+      <Avatar alt="profile picture" src={AvatarImg} />
     </ListItemButton>
   );
 };
